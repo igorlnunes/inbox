0439d4fc98b0b9b28ffca18c4b96eb1be923a5e4
Igor de Lima Nunes <igorlnunes@gmail.com>
2022-01-29 13:49:41 -0300

    First Steps

---
+++ compile.js
@@ -0,0 +1,12 @@
+// compile code will go here
+const path = require('path');
+const fs = require('fs');
+const solc = require('solc');
+
+const inboxPath = path.resolve(__dirname, 'contracts', 'Inbox.sol'); //dessa forma deixa mais inter
+///interoperacioanal
+
+const source = fs.readFileSync(inboxPath, 'utf8'); //le o conteudo do arquivo no endereço inboxPath
+
+module.exports = solc.compile(source, 1).contracts[':Inbox']; //chama a const solc e compila o conteudo da const source
+//exporta as funcoes
